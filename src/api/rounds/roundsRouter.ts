17c96f48a9d001a8eede13729b89b9c4f51bbda3
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-07-24 17:47:38 +0100

    chore: typo for rounds

--- src/api/rounds/roundsRouter.ts
+++ src/api/rounds/roundsRouter.ts
@@ -1,48 +1,48 @@
 // ts-ignore
-import express, { Router } from 'express';
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
-import { roundServiceInstance } from './roundsService';
-import { handleServiceResponse } from '@/common/utils/httpHandlers';
-import { createApiResponse } from '@/api-docs/openAPIResponseBuilders';
-import { z } from 'zod';
+import express, { Router } from "express";
+import { OpenAPIRegistry } from "@asteasolutions/zod-to-openapi";
+import { roundServiceInstance } from "./roundsService";
+import { handleServiceResponse } from "@/common/utils/httpHandlers";
+import { createApiResponse } from "@/api-docs/openAPIResponseBuilders";
+import { z } from "zod";
 
 export const roundsRegistry = new OpenAPIRegistry();
 export const roundsRouter: Router = express.Router();
 
 roundsRegistry.registerPath({
-  method: 'get',
-  path: 'rounds/rounds',
-  tags: ['signers'],
-  responses: createApiResponse(z.null(), 'Success')
+  method: "get",
+  path: "rounds/rounds",
+  tags: ["rounds"],
+  responses: createApiResponse(z.null(), "Success"),
 });
 
 roundsRegistry.registerPath({
-  method: 'get',
-  path: 'rounds/latest-round',
-  tags: ['signers'],
-  responses: createApiResponse(z.null(), 'Success')
+  method: "get",
+  path: "rounds/latest-round",
+  tags: ["rounds"],
+  responses: createApiResponse(z.null(), "Success"),
 });
 
 roundsRegistry.registerPath({
-  method: 'post',
-  path: 'rounds/rounds',
-  tags: ['signers'],
-  responses: createApiResponse(z.null(), 'Success')
+  method: "post",
+  path: "rounds/rounds",
+  tags: ["rounds"],
+  responses: createApiResponse(z.null(), "Success"),
 });
 
-roundsRouter.get('/rounds', async (req, res) => {
+roundsRouter.get("/rounds", async (req, res) => {
   const serviceResponse = await roundServiceInstance.findAll();
 
   return handleServiceResponse(serviceResponse, res);
 });
 
-roundsRouter.get('/latest-round', async (_req, res) => {
+roundsRouter.get("/latest-round", async (_req, res) => {
   const serviceResponse = await roundServiceInstance.fetchLatestRound();
 
   return handleServiceResponse(serviceResponse, res);
 });
 
-roundsRouter.post('/rounds', async (_req, res) => {
+roundsRouter.post("/rounds", async (_req, res) => {
   const serviceResponse = await roundServiceInstance.createRound();
 
   return handleServiceResponse(serviceResponse, res);
